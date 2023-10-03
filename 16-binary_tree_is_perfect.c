60ce5229bcfb5cee7fa3030713a6d528ba1c85fd
Nimo08 <nimomohamed800@gmail.com>
2023-10-03 19:02:09 +0300

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,19 +33,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
-	while (tree->left)
-	{
-		height++;
-		tree = tree->left;
-	}
-	while (tree->right)
-	{
-		height++;
-		tree = tree->right;
-	}
-	return (height);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
