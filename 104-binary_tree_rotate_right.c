fb6f5f93fb2d10da36cdc3f106c1626b4e376dbc
Nimo08 <nimomohamed800@gmail.com>
2023-10-05 13:08:56 +0300

    Task 23

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,23 +10,16 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *new_root = tree->left;
+	binary_tree_t *new_root;
 
 	if (tree == NULL || tree->left == NULL)
 	{
 		return (NULL);
 	}
-
+	new_root = tree->left;
 	tree->left = new_root->right;
 	new_root->right = tree;
-
-	if (tree->left)
-	{
-		tree->left->parent = tree;
-	}
-
-	new_root->parent = tree->parent;
 	tree->parent = new_root;
-
+	new_root->parent = NULL;
 	return (new_root);
 }
