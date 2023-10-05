5be1597e89a7ed2cb3b1bad7c10196fd271286f8
Nimo08 <nimomohamed800@gmail.com>
2023-10-05 14:09:31 +0300

    Task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -15,12 +15,18 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left)
+	if (node->parent->left == node)
 	{
-		return (node->parent->right);
+		if (node->parent->right == NULL)
+			return (NULL);
+		else
+			return (node->parent->right);
 	}
 	else
 	{
-		return (node->parent->left);
+		if (node->parent->left == NULL)
+			return (NULL);
+		else
+			return (node->parent->left);
 	}
 }
