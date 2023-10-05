04974779958a4aebba2033374c9650d6931b1bd1
JenMora <jeniffermourah73@gmail.com>
2023-10-05 11:42:02 +0300

    To check if a binary tree is a valid Binary Search Tree (BST), you can perform an in-order traversal of the tree while keeping track of the previously visited node's value. If, at any point during the traversal, you encounter a node whose value is less than or equal to the previously visited node's value, the tree is not a valid BST

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,50 @@
+#include "buinary_trees.h"
+
+/*helper function*/
+
+/**
+ *is_valid_bst_helper - this function is to perform the in-order traversal and check
+ *if the tree is a valid BST.
+ *@prev_value: to keep track of the previously visited node's value during the
+ *in-order traversal of the binary tree
+ *@node:represents the value stored in that node
+ *Return: 0 on success, otherwise 1
+ */
+int is_valid_bst_helper(const binary_tree_t *node, int *prev_value) {
+    if (node == NULL) {
+        return 1; 
+
+/**
+ * binary_tree_is_bst- a function that checks if a binary tree is a valid BST
+ * @tree: a pointer to the root node of the tree to check
+ * Return 1 on success, otherwise 0
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (node == NULL)
+	{
+		return (1);
+	}
+/*check the left subtree recursively*/
+	if (!is_valid_bst_helper(node->left, prev_value))
+	{
+		return (0);
+	}
+
+/**
+ * check if the current node's value is greater than the
+ * previous node's value
+ */
+	if (node->n <= *prev_value)
+	{
+		return (0);
+	}
+
+/* update the previous value to teh current node's value*/
+
+	*pre_value = node->n;
+
+/*check the right subtree recursively*/
+	return (is_valid_bst_helper(node->right, prev_value);
+}
